cadd304e0f74ba4a8a161e793f8fcbe9a4bf3bfa
Todd Haldeman <thaldeman@listrak.com>
2014-08-29 13:15:26 -0400

    small format improvement
    adds readability to split up string manipulation from actual
    value calculation.

--- rosalind.hs
+++ rosalind.hs
@@ -9,11 +9,12 @@ emitChar :: Char -> M.Map Char Int
 emitChar c = M.singleton c 1
 
 dna_nt_count :: T.Text -> String
-dna_nt_count content = intercalate " " $ map (show . snd) $ M.toAscList $ T.foldr (\c acc -> M.unionWith (+) acc (emitChar c)) M.empty content
+dna_nt_count content = intercalate " " $ map (show . snd) $ M.toAscList reducedValue
+	where reducedValue = T.foldr (\c acc -> M.unionWith (+) acc (emitChar c)) M.empty content
 
 main :: IO ()
 main = do
-	let fn = "count-dna-nt.txt"
+	let fn = "data.txt"
 	let fxn = dna_nt_count
 
 	TO.readFile fn >>= putStrLn . fxn 
