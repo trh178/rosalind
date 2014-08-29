b7c5fa8bc7dab014550b16f1a9d80d0c13aa797d
Todd Haldeman <thaldeman@listrak.com>
2014-08-29 14:03:24 -0400

    added dna to rna transcriber

--- rosalind.hs
+++ rosalind.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import Data.List (intercalate)
 import qualified Data.Map as M
 import qualified Data.Text as T
 import qualified Data.Text.IO as TO
@@ -8,15 +7,18 @@ import qualified Data.Text.IO as TO
 emitChar :: Char -> M.Map Char Int
 emitChar c = M.singleton c 1
 
-dna_nt_count :: T.Text -> String
-dna_nt_count content = intercalate " " $ map (show . snd) $ M.toAscList reducedValue
+dna_nt_count :: T.Text -> T.Text
+dna_nt_count content = T.intercalate (T.pack " ") $ map (T.pack . show . snd) $ M.toAscList reducedValue
 	where reducedValue = T.foldr (\c acc -> M.unionWith (+) acc (emitChar c)) M.empty content
 
+dna2rna :: T.Text -> T.Text
+dna2rna = T.replace (T.pack "T") (T.pack "U")
+	
 main :: IO ()
 main = do
 	let fn = "data.txt"
-	let fxn = dna_nt_count
+	let fxn = dna2rna
 
-	TO.readFile fn >>= putStrLn . fxn 
+	TO.readFile fn >>= TO.putStrLn . fxn 
 	
 	
